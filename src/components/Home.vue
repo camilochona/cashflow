7817cc30ce7bf20c29deae42dc7a459f2d15f10b
camilochona <juancamilocc09.jc@gmail.com>
2022-12-20 15:28:20 -0500

    Agragamos componente resume y movements

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,27 @@
 <template>
   <LayoutVue>
     <template #header> <HeaderVue></HeaderVue></template>
+    <template #resume>
+      <Resume></Resume>
+    </template>
+    <template #movements>
+      <Movements></Movements>
+    </template>
   </LayoutVue>
 </template>
 
 <script>
 import LayoutVue from "./Layout.vue";
 import HeaderVue from "./Header.vue";
+import Resume from "./Resume/index.vue";
+import Movements from "./Movements.vue";
+
 export default {
   components: {
     LayoutVue,
     HeaderVue,
+    Resume,
+    Movements,
   },
 };
 </script>
