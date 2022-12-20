cc2a8039ae7c2a2671818582c014e678cc2a3336
camilochona <juancamilocc09.jc@gmail.com>
2022-12-20 11:34:42 -0500

    Header y Layout

---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <LayoutVue>
+    <template #header> <HeaderVue></HeaderVue></template>
+  </LayoutVue>
+</template>
+
+<script>
+import LayoutVue from "./Layout.vue";
+import HeaderVue from "./Header.vue";
+export default {
+  components: {
+    LayoutVue,
+    HeaderVue,
+  },
+};
+</script>
